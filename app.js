5da479c4946faaace11d9b308df4325458f5e152
billoreankit66@gmail.com <billoreankit66@gmail.com>
2024-11-12 23:41:59 +0530

    app.js changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connectToDatabase = require('./config file/db.config');
 connectToDatabase()
+const userModel =require("./models/user.models")
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -11,8 +12,13 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get("/create", (req, res) => {
-  res.render('create');
+app.get("/create", async (req, res) => {
+  let user = await userModel.create({
+    name: "John Doe",
+    email: "john@example.com",
+    age: 25
+  })
+  res.send(user);
 })
 
 
